fd228a222a8dfa6ca1d4d4d7572d67d1a69353ac
ReidHoneycutt <30945205+ReidHoneycutt@users.noreply.github.com>
2023-04-24 13:18:38 -0700

    Create Find and Send Pose Data.py

---
+++ Find and Send Pose Data.py
@@ -0,0 +1,118 @@
+import socket
+import cv2
+import time
+import math
+from cvzone.PoseModule import PoseDetector
+import cv2
+
+cap = cv2.VideoCapture(0)
+detector = PoseDetector()
+while True:
+    success, img = cap.read()
+    img = detector.findPose(img)
+    lmList, bboxInfo = detector.findPosition(img, bboxWithHands=False)
+    if bboxInfo:
+        center = bboxInfo["center"]
+        cv2.circle(img, center, 5, (255, 0, 255), cv2.FILLED)
+
+    cv2.imshow("Image", img)
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+cap.release()
+cv2.destroyAllWindows()
+
+# initialize the video capture, detector, and socket objects
+def init_cap():
+    cap = cv2.VideoCapture(0)
+    return cap
+
+def init_detector():
+    detector = HandDetector(detectionCon=0.7, maxHands=2)
+    return detector
+
+def init_server():
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    s.bind(("127.0.0.1", 5000))  # the 5000 is the server ip that client is going to connect to.
+    s.listen(5)  # the queue
+    return s
+
+# functions to establish the client, and to send to the client
+def getClient(s):
+    while True:
+        clientsocket, Address = s.accept()
+        print(f'IP Address [{Address}] has connect to the server')
+        if clientsocket:
+            return clientsocket
+
+
+def send(clientsocket, packet):
+    clientsocket.send(packet)
+
+# constructs the packet of hand data to send to the client
+# the delimiter "@" seperates the coordinates, and the delimiter ":" seperates the two components of each coordinate
+def msg_construction(hands, max_w, max_h):
+    num_hands = 0
+    num_landmarks = 0
+    packet = ""
+    # if any hands are recognized, create a packet containing the byte representation of the total data length, number of hands, and number of landmarks
+    # the structure of a packet is "total data length + number of hands + number of landmarks + data of the hand coordinates"
+    if len(hands) > 0:
+        num_hands = 1
+        num_landmarks += len(hands[0]["lmList"])
+        for i in range(len(hands[0]["lmList"])):
+            if i < len(hands[0]["lmList"]) - 1:
+                packet += str(round(hands[0]["lmList"][i][0] / max_w, 2)) + ":" + str(round(hands[0]["lmList"][i][1] / max_h, 2)) + "@"
+            else:
+                packet += str(round(hands[0]["lmList"][i][0] / max_w, 2)) + ":" + str(round(hands[0]["lmList"][i][1] / max_h, 2))
+        if len(hands) > 1:
+            num_hands = 2
+            num_landmarks += len(hands[1]["lmList"])
+            for i in range(len(hands[0]["lmList"])):
+                if i < len(hands[0]["lmList"]) - 1:
+                    packet += str(round(hands[1]["lmList"][i][0] / max_w, 2)) + ":" + str(round(hands[1]["lmList"][i][1] / max_h, 2)) + "@"
+                else:
+                    packet += str(round(hands[1]["lmList"][i][0] / max_w, 2)) + ":" + str(round(hands[1]["lmList"][i][1] / max_h, 2))
+
+    data_len = len(packet).to_bytes(4, "big", signed=False)
+    num_hands = num_hands.to_bytes(1, "big", signed=False)
+    num_landmarks = num_landmarks.to_bytes(1, "big", signed=False)
+    data = packet.encode("utf-8")
+    packet = data_len + num_hands + num_landmarks + data
+    return packet
+
+# the function which runs continually throughout, searching for hands
+def search_for_hands(clientsocket, cap, detector):
+    max_w = 1
+    max_h = 1
+    while True:
+        success, img = cap.read()
+        hands, img = detector.findHands(img)
+        if len(hands) > 0:
+            if max(hands[0]["lmList"][:][0]) > max_w:
+                max_w = max(hands[0]["lmList"][:][0])
+
+            if max(hands[0]["lmList"][:][1]) > max_h:
+                max_h = max(hands[0]["lmList"][:][1])
+
+            if len(hands) > 1:
+                if max(hands[1]["lmList"][:][0]) > max_w:
+                    max_w = max(hands[0]["lmList"][:][0])
+
+                if max(hands[1]["lmList"][:][1]) > max_h:
+                    max_h = max(hands[0]["lmList"][:][1])
+
+        msg = msg_construction(hands, max_w, max_h)
+        send(clientsocket, msg)
+        cv2.waitKey(0)
+
+
+def main():
+    s = init_server()
+    c = getClient(s)
+    cap = init_cap()
+    detector = init_detector()
+    search_for_hands(c, cap, detector)
+
+
+if __name__ == '__main__':
+    main()
